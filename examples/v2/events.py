b829ad7c2e17368d9dd5b3559fc437d0ad15ec2f
NiceAesth <nice@aesth.dev>
2022-12-04 23:56:42 +0200

    Add event examples

---
+++ examples/v2/events.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import aiosu
+
+
+client = aiosu.v2.Client()
+
+
+@client.on_client_update
+async def on_client_update(event: aiosu.classes.events.ClientUpdateEvent):
+    """Called when client token is refreshed"""
+    print(f"Updated {event.client} {event.old_token} {event.new_token}")
+
+
+client_storage = aiosu.v2.ClientStorage()
+
+
+@client_storage.on_client_add
+async def on_client_add(event: aiosu.classes.events.ClientAddEvent):
+    """Called when client is added via client_storage.add_client() or client_storage.get_client()"""
+    print(f"{event.client_id} {event.client}")
+
+
+@client_storage.on_client_update
+async def on_client_update(event: aiosu.classes.events.ClientUpdateEvent):
+    """Called when any client token is refreshed"""
+    print(f"Updated {event.client} {event.old_token} {event.new_token}")
