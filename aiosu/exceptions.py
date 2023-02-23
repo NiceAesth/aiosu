40276d5ede9165d02b43f7400e6a264da53bb886
NiceAesth <nice@aesth.dev>
2023-02-23 23:15:41 +0200

    feat: add exception for get_client

--- aiosu/exceptions.py
+++ aiosu/exceptions.py
@@ -18,3 +18,14 @@ class APIException(Exception):
     def __init__(self, status: int, message: str = "") -> None:
         super().__init__(message)
         self.status = status
+
+
+class InvalidClientRequestedError(Exception):
+    """Invalid Client Requested Error
+
+    :param message: error message, defaults to ""
+    :type message: str, optional
+    """
+
+    def __init__(self, message: str = "") -> None:
+        super().__init__(message)
