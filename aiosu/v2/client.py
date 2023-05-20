31ef6b81765cce2b8444ab1b8364f48ae88c9baa
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2023-05-21 05:08:10 +0700

    fix: make token expiry timezone agnostic (#142)
    * Fix TypeError when checking token
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add try and except
    
    * style: remove unnecessary try except
    
    * fix: compare via timestamp
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Ig this should be on utc timestamp?
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: NiceAesth <8437201+NiceAesth@users.noreply.github.com>

--- aiosu/v2/client.py
+++ aiosu/v2/client.py
@@ -122,7 +122,7 @@ def check_token(func: F) -> F:
     @functools.wraps(func)
     async def _check_token(self: Client, *args: Any, **kwargs: Any) -> Any:
         token = await self.get_current_token()
-        if datetime.utcnow() > token.expires_on:
+        if datetime.utcnow().timestamp() > token.expires_on.timestamp():
             await self._refresh()
         return await func(self, *args, **kwargs)
 
