7b7da0a769266afa7f053707bf459b8c99cb4dbd
NiceAesth <nice@aesth.dev>
2023-09-20 16:41:25 +0300

    fix: decrease replay timestamp precision
    This caused issue with replays from lazer which have much higher precision on timestamps, causing values to potentially differ on export. This means the old workaround can be removed which was a major code smell of the old test.

--- tests/test_utils/test_replay.py
+++ tests/test_utils/test_replay.py
@@ -21,20 +21,17 @@ def replay_file(mode="osu"):
 
 def test_parse_replay(replay_file):
     for mode in modes:
-        data = BytesIO(replay_file(mode))
-        replay = aiosu.utils.replay.parse_file(data)
-        assert isinstance(replay, aiosu.models.ReplayFile)
+        with BytesIO(replay_file(mode)) as data:
+            replay = aiosu.utils.replay.parse_file(data)
+            assert isinstance(replay, aiosu.models.ReplayFile)
 
 
 def test_write_replay(replay_file):
     for mode in modes:
-        data = BytesIO(replay_file(mode))
-        replay = aiosu.utils.replay.parse_file(data)
-        rf = BytesIO()
-        aiosu.utils.replay.write_replay(rf, replay)
-        rf.seek(0)
-        new_replay = aiosu.utils.replay.parse_file(rf)
-        for attr in replay.model_fields:
-            if mode == "lazer" and attr == "played_at":
-                continue  # For some reason the played_at attribute is different by a couple seconds
-            assert getattr(replay, attr) == getattr(new_replay, attr)
+        with BytesIO(replay_file(mode)) as data:
+            replay = aiosu.utils.replay.parse_file(data)
+            with BytesIO() as f:
+                aiosu.utils.replay.write_replay(f, replay)
+                f.seek(0)
+                new_replay = aiosu.utils.replay.parse_file(f)
+                assert replay == new_replay
